2d13a5f9a96a8f6143f19218e95570247e0da9f2
freeangelsun <freeangelsun@gmail.com>
2024-12-20 17:19:59 +0900

    default module

--- exs/src/main/java/fps.exs/ExsApplication.java
+++ exs/src/main/java/fps.exs/ExsApplication.java
@@ -1,13 +1,13 @@
-package com.api;
+package fps.exs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiApplication {
+public class ExsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
+		SpringApplication.run(ExsApplication.class, args);
 	}
 
 }
