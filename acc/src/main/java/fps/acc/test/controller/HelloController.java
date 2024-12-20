3d2f5d218ca60bd32c4c698f9e8b43e96794a314
freeangelsun <freeangelsun@gmail.com>
2024-12-20 17:57:53 +0900

    default module

--- acc/src/main/java/fps/acc/test/controller/HelloController.java
+++ acc/src/main/java/fps/acc/test/controller/HelloController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/acc")
 public class HelloController {
 
-    @GetMapping("/hello")
+    @GetMapping("/test")
     public String sayHello() {
         return "Hello, World!";
     }
