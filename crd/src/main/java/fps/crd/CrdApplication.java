0fccfb860395f30c794561b44943f76627f13e2b
freeangelsun <freeangelsun@gmail.com>
2024-12-20 16:45:31 +0900

    default module

---
+++ crd/src/main/java/fps/crd/CrdApplication.java
@@ -0,0 +1,13 @@
+package fps.crd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrdApplication.class, args);
+	}
+
+}
---
+++ crd/src/test/java/fps/crd/CrdApplicationTests.java
@@ -0,0 +1,13 @@
+package fps.crd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
